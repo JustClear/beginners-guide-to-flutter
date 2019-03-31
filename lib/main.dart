7ce3c13c06c6457d49450b86727ffe096e66e417
大板栗 <576839360@qq.com>
2019-03-31 23:23:54 +0800

    📝 Update Demo

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,10 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Title',
-      home: Center(
-        child: Text(
-          '大板栗',
-          style: TextStyle(
-            color: Colors.white,
-            decoration: TextDecoration.none,
-          ),
-        ),
+    return Center(
+      child: Text(
+        '大板栗',
+        textDirection: TextDirection.ltr,
       ),
     );
   }
